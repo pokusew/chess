a21d081eb76710cb4515ec13a8530838d2187aca
Martin Endler <endlemar@fel.cvut.cz>
2021-03-28 12:22:41 +0200

    Add experimental drag and drop support to Square

--- src/main/java/cz/martinendler/chess/ui/Square.java
+++ src/main/java/cz/martinendler/chess/ui/Square.java
@@ -1,8 +1,10 @@
 package cz.martinendler.chess.ui;
 
 import cz.martinendler.chess.App;
+import javafx.event.EventHandler;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.*;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.ImagePattern;
@@ -11,6 +13,7 @@ import javafx.scene.shape.Rectangle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -42,6 +45,67 @@ public class Square extends StackPane {
 				)
 			)
 		));
+
+		setOnDragDetected((MouseEvent event) -> {
+
+			Dragboard db = startDragAndDrop(TransferMode.MOVE);
+
+			db.setDragView(
+				new Image(url, getWidth(), getHeight(), false, true, false),
+				// event coordinate system:
+				//
+				//    (0,0) -----> + event.getX()
+				//    |
+				//    |
+				//    V
+				//    +
+				//    event.getY()
+				//
+				// offset coordinate system:
+				//
+				//                       offsetY--
+				//    ++offsetX (drag view image center = (0,0)) offsetX--
+				//                       offsetY++
+				//
+				(getWidth() / 2) - event.getX(), -(getHeight() / 2) + event.getY()
+			);
+
+			log.info(MessageFormat.format(
+				"setOnDragDetected {0} {1} {2} {3}",
+				event.getX(), event.getY(),
+				getWidth(), getHeight()
+			));
+
+			ClipboardContent content = new ClipboardContent();
+			content.putString("source text");
+			db.setContent(content);
+
+		});
+
+		setOnMouseDragged((MouseEvent event) -> {
+			// event.setDragDetect(true);
+		});
+
+		setOnDragOver((DragEvent event) -> {
+
+			if (event.getGestureSource() != this && event.getDragboard().hasString()) {
+				event.acceptTransferModes(TransferMode.MOVE);
+			}
+
+			event.consume();
+
+		});
+
+		setOnDragDropped((DragEvent event) -> {
+			Dragboard db = event.getDragboard();
+			if (db.hasString()) {
+				log.info("Dropped: " + db.getString());
+				event.setDropCompleted(true);
+			} else {
+				event.setDropCompleted(false);
+			}
+			event.consume();
+		});
 
 
 	}
