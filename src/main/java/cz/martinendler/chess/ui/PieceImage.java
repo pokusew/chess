294c389aafdb14738b66ad77e5febed3ebde23eb
Martin Endler <endlemar@fel.cvut.cz>
2021-03-28 17:29:58 +0200

    Experiment with drag and drop (fix behavior)

--- src/main/java/cz/martinendler/chess/ui/PieceImage.java
+++ src/main/java/cz/martinendler/chess/ui/PieceImage.java
@@ -1,30 +1,24 @@
 package cz.martinendler.chess.ui;
 
 import cz.martinendler.chess.App;
-import javafx.scene.SnapshotParameters;
 import javafx.scene.image.Image;
-import javafx.scene.image.WritableImage;
-import javafx.scene.input.*;
-import javafx.scene.paint.Color;
+import javafx.scene.input.MouseDragEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.text.MessageFormat;
 
 
 public class PieceImage extends Rectangle {
 
 	public static final Logger log = LoggerFactory.getLogger(PieceImage.class);
 
-	private final int id;
+	public final int id;
 
 	private boolean dragging = false;
 	private double prevSceneX = 0;
 	private double prevSceneY = 0;
-	private double dragDetectedOffsetX = 0;
-	private double dragDetectedOffsetY = 0;
 
 	public PieceImage(int id) {
 		super();
@@ -47,23 +41,13 @@ public class PieceImage extends Rectangle {
 
 			// event.setDragDetect(true);
 
-			// setViewOrder(-1.0);
-			//
-			// if (orgSceneX == -1) {
-			// 	orgSceneX = event.getSceneX();
-			// 	orgSceneY = event.getSceneY();
-			// }
-			//
-
 			if (dragging) {
 
-				double offsetX = event.getSceneX() - prevSceneX;
-				double offsetY = event.getSceneY() - prevSceneY;
+				double changeX = event.getSceneX() - prevSceneX;
+				double changeY = event.getSceneY() - prevSceneY;
 
-				// log.info(MessageFormat.format("offsetX = {0}, offsetY = {1}", offsetX, offsetY));
-
-				setTranslateX(getTranslateX() + offsetX);
-				setTranslateY(getTranslateY() + offsetY);
+				setTranslateX(getTranslateX() + changeX);
+				setTranslateY(getTranslateY() + changeY);
 
 				prevSceneX = event.getSceneX();
 				prevSceneY = event.getSceneY();
@@ -73,14 +57,31 @@ public class PieceImage extends Rectangle {
 		});
 
 		setOnDragDetected((MouseEvent event) -> {
+
 			log.info("ID={} setOnDragDetected", id);
+
 			startFullDrag();
+
+			// here are different grab behaviors:
+			// TODO: choose what feels the most user-friendly
+
+			// 1) cursor is at the grab point
 			prevSceneX = event.getSceneX();
 			prevSceneY = event.getSceneY();
-			dragDetectedOffsetX = event.getX();
-			dragDetectedOffsetY = event.getY();
-			dragging = true;
+
+			// 2) align the node so that the cursor is always at its top-left corner
+			//   (no matter where the grab point was)
+			// prevSceneX = event.getSceneX() - event.getX();
+			// prevSceneY = event.getSceneY() - event.getY();
+
+			// 3) align the node so that the cursor is always at its middle
+			//    (no matter where the grab point was)
+			// prevSceneX = event.getSceneX() - event.getX() + getWidth() / 2;
+			// prevSceneY = event.getSceneY() - event.getY() + getHeight() / 2;
+
 			getParent().setViewOrder(-1);
+			dragging = true;
+
 		});
 
 		setOnMouseDragOver((MouseDragEvent event) -> {
@@ -88,11 +89,21 @@ public class PieceImage extends Rectangle {
 		});
 
 		setOnMouseDragReleased((MouseDragEvent event) -> {
+
 			log.info("ID={} setOnMouseDragReleased", id);
-			dragging = false;
-			getParent().setViewOrder(0);
-			setTranslateX(0);
-			setTranslateY(0);
+
+			if (dragging) {
+				dragging = false;
+				getParent().setViewOrder(0);
+				setTranslateX(0);
+				setTranslateY(0);
+			}
+
+			if (event.getGestureSource() != this && event.getGestureSource() instanceof PieceImage) {
+				PieceImage src = (PieceImage) event.getGestureSource();
+				log.info("ID={} setOnMouseDragReleased {}-->{}", id, src.id, id);
+			}
+
 		});
 
 		setOnMouseDragEntered((MouseDragEvent event) -> {
@@ -104,22 +115,31 @@ public class PieceImage extends Rectangle {
 		});
 
 		setOnMousePressed((MouseEvent event) -> {
+
 			log.info("ID={} onMousePressed", id);
-			// setMouseTransparent(true);
+
+			setMouseTransparent(true);
+			getParent().setMouseTransparent(true);
+
 		});
 
 		setOnMouseReleased((MouseEvent event) -> {
+
 			log.info("ID={} onMouseReleased", id);
-			// setMouseTransparent(false);
+
+			setMouseTransparent(false);
+			getParent().setMouseTransparent(false);
+
 			if (dragging) {
+				dragging = false;
 				getParent().setViewOrder(0);
 				setTranslateX(0);
 				setTranslateY(0);
 			}
+
 		});
 
 	}
-
 
 	@Override
 	public double minWidth(double height) {
