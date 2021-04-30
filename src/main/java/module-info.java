cc585743363a948dd154109b520d216425864d4d
Martin Endler <endlemar@fel.cvut.cz>
2021-04-30 03:50:38 +0200

    Fix module-info.java for tests

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -11,6 +11,10 @@ module chess {
 	opens cz.martinendler.chess.ui;
 	opens cz.martinendler.chess.ui.controllers;
 
+	opens cz.martinendler.chess.pgn.entity;
+	opens cz.martinendler.chess.pgn.antlr4;
+	opens cz.martinendler.chess.pgn;
+
 	exports cz.martinendler.chess;
 
 }
