3c1a14c2f1e077626d8378cc8dd6489148ccb492
Martin Endler <endlemar@fel.cvut.cz>
2021-05-04 03:24:39 +0200

    Improve Constants (comments and cleanup)

--- src/main/java/cz/martinendler/chess/engine/Constants.java
+++ src/main/java/cz/martinendler/chess/engine/Constants.java
@@ -4,119 +4,141 @@ import cz.martinendler.chess.engine.board.Square;
 import cz.martinendler.chess.engine.move.Move;
 import cz.martinendler.chess.engine.pieces.Piece;
 
-import java.util.*;
+import java.util.EnumMap;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
- * The type Constants.
+ * Chess game constants (such as starting positions, castings, pieces' notation, ...)
  */
 public class Constants {
 
 	/**
-	 * The constant startStandardFENPosition.
+	 * Standard chess starting position as a FEN string
 	 */
 	public static final String startStandardFENPosition = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+
 	/**
-	 * The constant DEFAULT_WHITE_OO.
+	 * The move of the white king as a part of the kingside castling (the short one)
 	 */
 	public static final Move DEFAULT_WHITE_OO = new Move(Square.E1, Square.G1);
 	/**
-	 * The constant DEFAULT_WHITE_OOO.
+	 * The move of the white king as a part of the queenside castling (the long one)
 	 */
 	public static final Move DEFAULT_WHITE_OOO = new Move(Square.E1, Square.C1);
+
 	/**
-	 * The constant DEFAULT_BLACK_OO.
+	 * The move of the black king as a part of the kingside castling (the short one)
 	 */
 	public static final Move DEFAULT_BLACK_OO = new Move(Square.E8, Square.G8);
 	/**
-	 * The constant DEFAULT_BLACK_OOO.
+	 * The move of the black king as a part of the queenside castling (the long one)
 	 */
 	public static final Move DEFAULT_BLACK_OOO = new Move(Square.E8, Square.C8);
+
 	/**
-	 * The constant DEFAULT_WHITE_ROOK_OO.
+	 * The move of the white rook as a part of the kingside castling (the short one)
 	 */
 	public static final Move DEFAULT_WHITE_ROOK_OO = new Move(Square.H1, Square.F1);
 	/**
-	 * The constant DEFAULT_WHITE_ROOK_OOO.
+	 * The move of the white rook as a part of the queenside castling (the long one)
 	 */
 	public static final Move DEFAULT_WHITE_ROOK_OOO = new Move(Square.A1, Square.D1);
+
 	/**
-	 * The constant DEFAULT_BLACK_ROOK_OO.
+	 * The move of the black rook as a part of the kingside castling (the short one)
 	 */
 	public static final Move DEFAULT_BLACK_ROOK_OO = new Move(Square.H8, Square.F8);
 	/**
-	 * The constant DEFAULT_BLACK_ROOK_OOO.
+	 * The move of the black rook as a part of the queenside castling (the long one)
 	 */
 	public static final Move DEFAULT_BLACK_ROOK_OOO = new Move(Square.A8, Square.D8);
-	/**
-	 * The constant DEFAULT_WHITE_OO_SQUARES.
-	 */
-	public static final List<Square> DEFAULT_WHITE_OO_SQUARES = new ArrayList<Square>();
-	/**
-	 * The constant DEFAULT_WHITE_OOO_SQUARES.
-	 */
-	public static final List<Square> DEFAULT_WHITE_OOO_SQUARES = new ArrayList<Square>();
-	/**
-	 * The constant DEFAULT_BLACK_OO_SQUARES.
-	 */
-	public static final List<Square> DEFAULT_BLACK_OO_SQUARES = new ArrayList<Square>();
-	/**
-	 * The constant DEFAULT_BLACK_OOO_SQUARES.
-	 */
-	public static final List<Square> DEFAULT_BLACK_OOO_SQUARES = new ArrayList<Square>();
 
 	/**
-	 * The constant DEFAULT_WHITE_OO_ALL_SQUARES.
+	 * The inner squares between the king and the rook
+	 * in the white's kingside castling (the short one)
 	 */
-	public static final List<Square> DEFAULT_WHITE_OO_ALL_SQUARES = new ArrayList<Square>();
+	public static final List<Square> DEFAULT_WHITE_OO_SQUARES = List.of(
+		Square.F1,
+		Square.G1
+	);
 	/**
-	 * The constant DEFAULT_WHITE_OOO_ALL_SQUARES.
+	 * The inner squares between the king and the rook
+	 * in the white's queenside castling (the long one)
 	 */
-	public static final List<Square> DEFAULT_WHITE_OOO_ALL_SQUARES = new ArrayList<Square>();
+	public static final List<Square> DEFAULT_WHITE_OOO_SQUARES = List.of(
+		Square.D1,
+		Square.C1
+		// TODO: why not also Square.B1?
+	);
+
 	/**
-	 * The constant DEFAULT_BLACK_OO_ALL_SQUARES.
+	 * The inner squares between the king and the rook
+	 * in the black's kingside castling (the short one)
 	 */
-	public static final List<Square> DEFAULT_BLACK_OO_ALL_SQUARES = new ArrayList<Square>();
+	public static final List<Square> DEFAULT_BLACK_OO_SQUARES = List.of(
+		Square.F8,
+		Square.G8
+	);
 	/**
-	 * The constant DEFAULT_BLACK_OOO_ALL_SQUARES.
+	 * The inner squares between the king and the rook
+	 * in the black's queenside castling (the long one)
 	 */
-	public static final List<Square> DEFAULT_BLACK_OOO_ALL_SQUARES = new ArrayList<Square>();
+	public static final List<Square> DEFAULT_BLACK_OOO_SQUARES = List.of(
+		Square.D8,
+		Square.C8
+		// TODO: why not also Square.B1?
+	);
+
+	/**
+	 * The all (incl. the king and the rook) squares between the king and the rook
+	 * in the white's kingside castling (the short one)
+	 */
+	public static final List<Square> DEFAULT_WHITE_OO_ALL_SQUARES = List.of(
+		Square.F1,
+		Square.G1
+	);
+	/**
+	 * The all (incl. the king and the rook) squares between the king and the rook
+	 * in the white's queenside castling (the long one)
+	 */
+	public static final List<Square> DEFAULT_WHITE_OOO_ALL_SQUARES = List.of(
+		Square.D1,
+		Square.C1,
+		Square.B1
+	);
+
+	/**
+	 * The all (incl. the king and the rook) squares between the king and the rook
+	 * in the black's kingside castling (the short one)
+	 */
+	public static final List<Square> DEFAULT_BLACK_OO_ALL_SQUARES = List.of(
+		Square.F8,
+		Square.G8
+	);
+	/**
+	 * The all (incl. the king and the rook) squares between the king and the rook
+	 * in the black's queenside castling (the long one)
+	 */
+	public static final List<Square> DEFAULT_BLACK_OOO_ALL_SQUARES = List.of(
+		Square.D8,
+		Square.C8,
+		Square.B8
+	);
 
 	/**
 	 * The constant pieceNotation.
 	 */
-	public static final EnumMap<Piece, String> pieceNotation =
-		new EnumMap<Piece, String>(Piece.class);
+	public static final EnumMap<Piece, String> pieceNotation = new EnumMap<>(Piece.class);
 	/**
 	 * The constant pieceNotationR.
 	 */
-	public static final Map<String, Piece> pieceNotationR =
-		new HashMap<String, Piece>(12);
+	public static final Map<String, Piece> pieceNotationR = new HashMap<>(12);
 
 	public static final Move emptyMove = new Move(Square.NONE, Square.NONE);
 
 	static {
-
-		DEFAULT_WHITE_OO_SQUARES.add(Square.F1);
-		DEFAULT_WHITE_OO_SQUARES.add(Square.G1);
-		DEFAULT_WHITE_OOO_SQUARES.add(Square.D1);
-		DEFAULT_WHITE_OOO_SQUARES.add(Square.C1);
-
-		DEFAULT_BLACK_OO_SQUARES.add(Square.F8);
-		DEFAULT_BLACK_OO_SQUARES.add(Square.G8);
-		DEFAULT_BLACK_OOO_SQUARES.add(Square.D8);
-		DEFAULT_BLACK_OOO_SQUARES.add(Square.C8);
-
-		DEFAULT_WHITE_OO_ALL_SQUARES.add(Square.F1);
-		DEFAULT_WHITE_OO_ALL_SQUARES.add(Square.G1);
-		DEFAULT_WHITE_OOO_ALL_SQUARES.add(Square.D1);
-		DEFAULT_WHITE_OOO_ALL_SQUARES.add(Square.C1);
-		DEFAULT_WHITE_OOO_ALL_SQUARES.add(Square.B1);
-
-		DEFAULT_BLACK_OO_ALL_SQUARES.add(Square.F8);
-		DEFAULT_BLACK_OO_ALL_SQUARES.add(Square.G8);
-		DEFAULT_BLACK_OOO_ALL_SQUARES.add(Square.D8);
-		DEFAULT_BLACK_OOO_ALL_SQUARES.add(Square.C8);
-		DEFAULT_BLACK_OOO_ALL_SQUARES.add(Square.B8);
 
 		pieceNotation.put(Piece.WHITE_PAWN, "P");
 		pieceNotation.put(Piece.WHITE_KNIGHT, "N");
@@ -146,10 +168,11 @@ public class Constants {
 	}
 
 	private Constants() {
+		// we do not want Constants to be instantiable
 	}
 
 	/**
-	 * gets the notation of a piece
+	 * Gets the notation of a piece
 	 *
 	 * @param piece the piece
 	 * @return piece notation
@@ -159,7 +182,7 @@ public class Constants {
 	}
 
 	/**
-	 * gets the piece by its notation
+	 * Gets the piece by its notation
 	 *
 	 * @param notation the notation
 	 * @return piece by notation
