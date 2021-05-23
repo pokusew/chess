3c80be8b7e22dbbac9b76edb77459bf5d7334619
Martin Endler <endlemar@fel.cvut.cz>
2021-05-23 08:06:01 +0200

    [ui] Implement support for customizable startup window position via custom VM option / env variable

--- src/main/java/cz/martinendler/chess/App.java
+++ src/main/java/cz/martinendler/chess/App.java
@@ -8,6 +8,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 import javafx.util.Pair;
+import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -66,6 +67,105 @@ public class App extends Application {
 		launch(args);
 	}
 
+	public static final class StartupPosition {
+
+		private final int x;
+		private final int y;
+
+		public StartupPosition(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+
+		public int getX() {
+			return x;
+		}
+
+		public int getY() {
+			return y;
+		}
+
+	}
+
+	/**
+	 * Parses the custom window startup position option
+	 * <p>
+	 * The custom window startup position can be specified as java VM argument/option:
+	 * For example, the following option
+	 * <pre>
+	 *     -Dcz.martinendler.chess.position=-1500:100
+	 * <pre/>
+	 * corresponds to the position x=-1500, y=100.
+	 *
+	 * @param value the value of {@code cz.martinendler.chess.position} option
+	 * @return {@link StartupPosition} the custom startup position, or {@code null} if there was an parsing error
+	 */
+	public static @Nullable StartupPosition parseCustomWindowStartupPosition(@Nullable String value) {
+
+		if (value == null || value.isEmpty()) {
+			return null;
+		}
+
+		String[] parts = value.split(":");
+
+		if (parts.length != 2) {
+			log.error("parseCustomWindowStartupPosition: invalid value '" + value + "'");
+			return null;
+		}
+
+		try {
+
+			int x = Integer.parseInt(parts[0]);
+			int y = Integer.parseInt(parts[1]);
+
+			return new StartupPosition(x, y);
+
+		} catch (NumberFormatException e) {
+
+			log.error("parseCustomWindowStartupPosition: invalid value '" + value + "'");
+			return null;
+
+		}
+
+	}
+
+	/**
+	 * Tries to load and parse the option for custom window startup position
+	 * <p>
+	 * The custom window startup position can be specified as:
+	 * 1. Java VM argument/option {@code cz.martinendler.chess.position}
+	 * 2. Environment variable {@code CHESS_UI_POSITION}
+	 * If both are set, {@code cz.martinendler.chess.position} takes precedence.
+	 * <p>
+	 * The syntax of the value is x:y. For example, passing the option
+	 * {@code -Dcz.martinendler.chess.position=-1500:100}, sets the startup position to x=-1500, y=100.
+	 *
+	 * @return {@link StartupPosition} the custom startup position, or {@code null} if there was an error
+	 */
+	public static @Nullable StartupPosition getCustomWindowStartupPosition() {
+
+		String position = null;
+
+		// first try the VM argument/option
+		try {
+			position = System.getProperty("cz.martinendler.chess.position", null);
+		} catch (SecurityException e) {
+			log.error("an error occurred while accessing cz.martinendler.chess.position property", e);
+		}
+
+		// fallback to the env variable
+		if (position == null) {
+			try {
+				position = System.getenv("CHESS_UI_POSITION");
+			} catch (SecurityException e) {
+				log.error("an error occurred while accessing CHESS_UI_POSITION env variable", e);
+			}
+		}
+
+		return parseCustomWindowStartupPosition(position);
+
+	}
+
 	@Override
 	public void start(Stage stage) {
 
@@ -74,20 +174,25 @@ public class App extends Application {
 		primaryStage = stage;
 		primaryStage.setTitle("Chess");
 
-		// TODO: remove
-		// show the main window in the second display for easier developing
-		// primaryStage.setX(-1500);
-		// primaryStage.setY(100);
+		// allows easily change the main window position
+		// may be useful during development with a second display
+		StartupPosition position = getCustomWindowStartupPosition();
+		if (position != null) {
+			log.info("primary stage custom x={} y={}", position.x, position.y);
+			primaryStage.setX(position.getX());
+			primaryStage.setY(position.getY());
+		}
 
+		// center the main window on the primary screen (default behavior, no need for custom code)
 		// Rectangle2D primScreenBounds = Screen.getPrimary().getVisualBounds();
 		// stage.setX((primScreenBounds.getWidth() - stage.getWidth()) / 2);
 		// stage.setY((primScreenBounds.getHeight() - stage.getHeight()) / 2);
 
-		initRootLayout();
+		initRootScene();
 
 	}
 
-	private void initRootLayout() {
+	private void initRootScene() {
 
 		Pair<Parent, AppAwareController> root = loadFXML("view/game");
 
